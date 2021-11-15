2dbe734596ca79b66f940716d6a7de164aedf980
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-11-16 01:34:00 +0200

    ProductList and ProductItem classes

---
+++ js/ProductList.js
@@ -0,0 +1,54 @@
+export class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];
+        this._fetchGoods();
+    }
+
+    _fetchGoods() {
+        // Parse JSON from server
+        this.goods = [
+            {id: 1, name: 'Notebook', price: 1000},
+            {id: 2, name: 'Mouse', price: 100},
+            {id: 3, name: 'Keyboard', price: 250},
+            {id: 4, name: 'Gamepad', price: 150},
+        ];
+    }
+
+    _initProducts() {
+        for(const good of this.goods) {
+            this.allProducts.push(new ProductItem(good.id, good.name, good.price))
+        }
+    }
+
+    get price() {
+        return this.allProducts.reduce((a, b) => a + b.price, 0)
+    }
+
+    render() {
+        const block  = document.querySelector(this.container);
+        this._initProducts()
+        return block.insertAdjacentHTML('beforeend', this.allProducts.map(item => item.markup).join(''));
+    }
+}
+
+
+export class ProductItem {
+    constructor(id, title, price, img = 'https://placehold.it/200x125') {
+        this.title = title;
+        this.price = price;
+        this._id = id;
+        this._img = img;
+    }
+
+    get markup() {
+        return `<div class="product-item">
+             <img src=${this._img} alt="picture">
+             <h3>${this.title}</h3>
+             <p>${this.price}</p>
+             <button class="by-btn">Добавить</button>
+         </div>`;
+    }
+
+}
--- js/main.js
+++ js/main.js
@@ -1,20 +1,9 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 1000},
-  {id: 2, title: 'Mouse', price: 100},
-  {id: 3, title: 'Keyboard', price: 250},
-  {id: 4, title: 'Gamepad', price: 150},
-];
+import {ProductList} from "./ProductList.js";
+//import {Cart} from "./Cart.js";
 
-const renderProduct = (product, img='https://placehold.it/200x125') => `<div class="product-item">
-            <img src=${img} alt="picture">
-            <h3>${product.title}</h3>
-            <p>${product.price}</p>
-            <button class="by-btn">Добавить</button>
-          </div>`;
+'use strict';
 
-const renderProducts = list => document.querySelector('.products')
-    .innerHTML = list
-      .map(item => renderProduct(item))
-      .join('\n');
+const products = new ProductList();
+products.render();
 
-renderProducts(products);
+console.log(products.price)
