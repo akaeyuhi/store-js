e01b4dc1cd1b6d71ca3c6e73d38e3e7bfce8f391
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-11-18 00:35:23 +0200

    Base product render

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
-import {ProductList, Cart} from "./components/List.js";
+import {ProductItem} from "./components/Item.js";
+import {constants} from "./components/constants.js";
+import {List} from "./components/List.js";
 
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const cart = new Cart();
-    const products = new ProductList(cart);
-    console.log(products.price, products.toString())
+new Vue({
+    el: '#app',
+    data: {
+        goods: [],
+        filteredGoods: [],
+        allProducts: [],
+        searchLine: ''
+    },
+
+    methods: {
+        getItems: List.prototype.getItems,
+        _initProducts() {
+            for(const good of this.goods) {
+                this.allProducts.push(
+                    new ProductItem(good)
+                )
+            }
+        }
+    },
+    mounted() {
+        this.getItems(constants.API + '/catalogData.json').then(data => {
+            this.goods = [...data];
+            this._initProducts()
+        })
+    }
 })
 
