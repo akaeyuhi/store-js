ff7a2a36ffe8ed41d80b176656879a25d990f551
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-11-26 01:29:14 +0200

    Logic for router.js

---
+++ server/router.js
@@ -0,0 +1,22 @@
+"use strict";
+const express = require('express');
+const fs = require('fs/promises');
+const handler = require('./handler');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    fs.readFile('./server/db/userCart.json', 'utf-8')
+        .then(data => res.send(data))
+        .catch(err => res.sendStatus(404, JSON.stringify({result: 0, text: err}))
+);
+});
+
+router.post('/', (req, res) => {
+    handler(req, res, 'add', './server/db/userCart.json');
+});
+
+router.put('/:id', (req, res) => {
+    handler(req, res, 'change', './server/db/userCart.json');
+});
+
+module.exports = router;
