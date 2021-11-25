549c5e822e40175965b99dcc47f75b45c7810e3c
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-11-26 01:26:14 +0200

    package.json, basic server logic

---
+++ server/server.js
@@ -0,0 +1,24 @@
+"use strict";
+const express = require("express");
+const fs = require("fs/promises");
+const router = require("./router.js");
+const app = express();
+
+app.use(express.json());
+app.use('/', express.static('./client'));
+app.use('/api/cart', router);
+
+app.get('/api/products', async (req, res) => {
+    fs.readFile('./server/db/products.json', 'utf-8')
+        .then(data => {
+            res.send(data);
+    })
+        .catch(err => {
+        res.send(JSON.stringify({result: 0, text: err}));
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
