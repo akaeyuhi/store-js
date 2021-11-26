32f30ee00ab6937d06c1949a9ea35ce8211bfc87
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-11-26 02:50:12 +0200

    Wrote request logic for main.js

--- client/js/main.js
+++ client/js/main.js
@@ -10,6 +10,45 @@ new Vue({
                     console.log(error);
                 })
         },
+        putRequest(url, data) {
+            return fetch(url, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError(error);
+                })
+        },
+        postRequest(url, data) {
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError(error);
+                })
+        },
+        deleteRequest(url, data) {
+            return fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.setError(error);
+                })
+        },
         filter(queryString) {
             let regexp = new RegExp(queryString, 'i');
             if (queryString !== '')
